1c341197ac7d3083d9e0eca3628b7eabeaf4fb89
Jacob <jacobodonnell96@proton.me>
2024-09-21 14:46:59 -0500

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import Navbar from './Navbar.jsx';
+import Hero from './Hero.jsx';
+import Sidebar from './Sidebar.jsx';
+import Submenu from './Submenu.jsx';
+
 const App = () => {
-  return <h2>Strapi Starter</h2>;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  );
 };
 export default App;
